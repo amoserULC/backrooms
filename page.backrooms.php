1bfd271f2aa60bcad7d69698e7bc0b1690f410f0
amoserULC <amoser@uplync.com>
2026-03-18 16:03:32 -0400

    Update and rename page.helloworld.php to page.backrooms.php

--- page.backrooms.php
+++ page.backrooms.php
@@ -1,3 +1,4 @@
 <?php
 //This guy calls the initial page through showPage function in Helloworld.class.php
+//MAKE SURE the file-name matches the namespace label used by the entire module in module.xml
 echo FreePBX::create()->Helloworld->showPage();
