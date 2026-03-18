084057fc951e724daa48999af6d63e31d698cc4d
amoserULC <amoser@uplync.com>
2026-03-18 16:37:25 -0400

    Update bootnav.php - remove helloworld string

--- views/bootnav.php
+++ views/bootnav.php
@@ -1,2 +1,2 @@
-<a href="?display=helloworld" class="list-group-item"><i class="fa fa-list"></i>&nbsp;<?php echo _("List Notes")?></a>
-<a href="?display=helloworld&view=form" class="list-group-item"><i class="fa fa-plus"></i>&nbsp;<?php echo _("Add Note")?></a>
+<a href="?display=backrooms" class="list-group-item"><i class="fa fa-list"></i>&nbsp;<?php echo _("List Notes")?></a>
+<a href="?display=backrooms&view=form" class="list-group-item"><i class="fa fa-plus"></i>&nbsp;<?php echo _("Add Note")?></a>
