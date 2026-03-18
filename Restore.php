045bd29dc45ffb8f76c04457fdbada4bdd1d3d05
amoserULC <amoser@uplync.com>
2026-03-18 16:17:42 -0400

    Update Restore.php

--- Restore.php
+++ Restore.php
@@ -5,7 +5,7 @@ class Restore Extends Base\RestoreBase{
 	public function runRestore($jobid){
 		$settings = $this->getConfigs();
 		foreach ($settings as $key => $value) {
-			$this->freepbx->Helloworld->setMultiConfig($value, $key);
+			$this->freepbx->Backrooms->setMultiConfig($value, $key);
 		}
 	}
 	public function processLegacy($pdo, $data, $tables, $unknownTables, $tmpfiledir){
