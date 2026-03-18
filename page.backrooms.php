3f1b0c5dd0b6d1738f84b5b3cb5edce7f1c129a3
amoserULC <amoser@uplync.com>
2026-03-18 16:14:18 -0400

    Update page.backrooms.php
    Updated class name to match Backrooms.class.php

--- page.backrooms.php
+++ page.backrooms.php
@@ -1,4 +1,5 @@
 <?php
 //This guy calls the initial page through showPage function in Helloworld.class.php
 //MAKE SURE the file-name matches the namespace label used by the entire module in module.xml
-echo FreePBX::create()->Helloworld->showPage();
+//Also, the name called here has to match the class name in Backrooms.class.php
+echo FreePBX::create()->Backrooms->showPage();
