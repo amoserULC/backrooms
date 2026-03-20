6095dfeff602c08b4c21658f7147d48f9a18e8e9
amoserULC <amoser@uplync.com>
2026-03-20 09:24:32 -0400

    Update grid.php - remove hello world string

--- views/grid.php
+++ views/grid.php
@@ -1,4 +1,4 @@
-<table id="hwgrid" data-url="ajax.php?module=helloworld&command=getJSON&jdata=grid" data-cache="false" data-height="299" data-toggle="table" class="table table-striped">
+<table id="hwgrid" data-url="ajax.php?module=backrooms&command=getJSON&jdata=grid" data-cache="false" data-height="299" data-toggle="table" class="table table-striped">
 	<thead>
 		<tr>
 			<th data-field="id" data-formatter="linkFormat" class="col-md-1"><?php echo _("Action")?></th>
